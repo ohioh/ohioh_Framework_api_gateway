b354ba363b5c930f69a5503fa90559f0b48a619d
shaokat <shaokatcse@gmail.com>
2020-05-18 17:22:41 +0200

    jwt authentication added

---
+++ go.mod
@@ -0,0 +1,9 @@
+module githgithub.com/elliotforbes/go-jwt-ohioh/client
+
+go 1.14
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/gorilla/mux v1.7.4
+	github.com/joho/godotenv v1.3.0
+)
--- main.go
+++ main.go
@@ -7,7 +7,9 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
+	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
 )
@@ -118,13 +120,74 @@ func insertUserLocationRecord(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// jwt authenticaton
+
+var mySigningKey = []byte(os.Getenv("SIGNING_KEY"))
+
+func getToken(w http.ResponseWriter, r *http.Request) {
+	validToken, err := GenerateJWT()
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+
+	fmt.Fprintf(w, validToken)
+}
+
+func GenerateJWT() (string, error) {
+	token := jwt.New(jwt.SigningMethodHS256)
+
+	claims := token.Claims.(jwt.MapClaims)
+
+	claims["authorized"] = true
+	claims["user"] = "Ellit Forbes"
+	claims["exp"] = time.Now().Add(time.Minute * 30).Unix()
+
+	tokenString, err := token.SignedString(mySigningKey)
+
+	if err != nil {
+		fmt.Errorf("Something wernt wrong: %s", err.Error())
+		return "", err
+	}
+
+	return tokenString, nil
+
+}
+
+//token verification
+
+func isAuthorized(endpoint func(http.ResponseWriter, *http.Request)) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Header["Token"] != nil {
+			token, err := jwt.Parse(r.Header["Token"][0], func(token *jwt.Token) (interface{}, error) {
+
+				if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+					return nil, fmt.Errorf("There was an error")
+				}
+				return mySigningKey, nil
+			})
+			if err != nil {
+				fmt.Fprintf(w, err.Error())
+			}
+
+			if token.Valid {
+				endpoint(w, r)
+			}
+
+		} else {
+			fmt.Fprintf(w, "Not Authorized")
+		}
+	})
+}
+
 func handleRequests() {
 	gatewayPrefix := "/gateway/ohioh"
 	gatewayRouter := mux.NewRouter().StrictSlash(true)
 	gatewayRouter.HandleFunc(gatewayPrefix+"/", defaultRoute)
-	gatewayRouter.HandleFunc(gatewayPrefix+"/users", allUsers).Methods(http.MethodGet)
+	gatewayRouter.HandleFunc(gatewayPrefix+"/users", isAuthorized(allUsers)).Methods(http.MethodGet)
+	//http.Handle(gatewayPrefix+"/users", isAuthorized(allUsers))
 	gatewayRouter.HandleFunc(gatewayPrefix+"/users", insertUserRecord).Methods(http.MethodPost)
 	gatewayRouter.HandleFunc(gatewayPrefix+"/user_locations", insertUserLocationRecord).Methods(http.MethodPost)
+	gatewayRouter.HandleFunc(gatewayPrefix+"/token", getToken)
 	log.Fatal(http.ListenAndServe(":8000", gatewayRouter))
 }
 
